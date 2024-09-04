cc87e6dfc34a3fc1047452cc43c714d00954f99c
Noah <cramernoah8@gmail.com>
2024-09-04 10:08:20 -0400

    more code changes

--- learning01/main.gd
+++ learning01/main.gd
@@ -17,42 +17,45 @@ func _on_line_edit_text_submitted(new_text):
 	if new_text == "help":
 		text_edit.clear()
 		
-		text_history.text =text_history.text + "\n\n/help command used\n\nList of commands:\nhelp\nname player\nsearch room\nend game\nlook at\npick up" 
+		text_history.text =text_history.text + "\n\n*help command used\n\n*List of commands:\n*help\n*name player\n*search room\n*end game\n*look at\n*pick up" 
 		text_edit.clear()
 		
-	elif new_text == "/name player":
+	elif new_text == "name player":
 		text_edit.clear()
 		print("start to name the player")
-	elif new_text == "search room":
+	elif new_text == "look up":
+		text_history.text =text_history.text + "\n\n*you look up\n\n*you look upward at the ceiling"
 		text_edit.clear()
-		print("you look around the room")
+	elif new_text == "look down":
+		text_history.text =text_history.text + "\n\n*you look down\n\n*you look downward at the floor"
+		text_edit.clear()
 	elif new_text == "end game":
 		text_edit.clear()
 		get_tree().quit()
 	elif new_text == "look at":
-		text_history.text =text_history.text + "\n\nyou look, but there is nothing in your hand...\n\n(use \"pick up\" to hold an item in your hand)"
+		text_history.text =text_history.text + "\n\n*you look, but there is nothing in your hand...\n\n(use \"pick up\" to hold an item in your hand)"
 		text_edit.clear()
 	elif new_text == "look north":
-		text_history.text = text_history.text + "\n\nyou look north"
+		text_history.text = text_history.text + "\n\n*you look north"
 		text_edit.clear()
 	elif new_text == "look south":
-		text_history.text = text_history.text + "\n\nyou look south"
+		text_history.text = text_history.text + "\n\n*you look south"
 		text_edit.clear()
 	elif new_text == "look east":
-		text_history.text = text_history.text + "\n\nyou look east"
+		text_history.text = text_history.text + "\n\n*you look east"
 		text_edit.clear()
 	elif new_text == "look west":
-		text_history.text = text_history.text + "\n\nyou look west"
+		text_history.text = text_history.text + "\n\n*you look west"
 		text_edit.clear()
 	elif new_text == "pick up" && searched_room == true:
-		text_history.text =text_history.text + "\n\nyou reach out, but there is nothing to grab as you havent \"searched\" \n\n(use the search before using pick up items nearby)"
+		text_history.text =text_history.text + "\n\n*you reach out, but there is nothing to grab as you havent \"searched\" \n\n(use the search before using pick up items nearby)"
 		text_edit.clear()
 	elif new_text == "search":
 		searched_room = true
-		text_history.text =text_history.text + "\n\nyou look around the room and find some items\n\nyou find a \nbook\ncandle\ncandle\nstick\na deck of playing cards"
+		text_history.text =text_history.text + "\n\n*you look around the room and find some items\n\n*you find a \n*book\n*candle\n*candle\n*stick\n*a deck of playing cards"
 		text_edit.clear()
 		print(searched_room)
 	
 	else: 
 		text_edit.clear()
-		text_history.text =text_history.text + "\n\nthat is not a command\n\nif you need help,\"help\"will help you with a list of commands! "
+		text_history.text =text_history.text + "\n\n*that is not a command\n\n*if you need help,\"help\"will help you with a list of commands! "
