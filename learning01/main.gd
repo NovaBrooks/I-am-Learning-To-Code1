e116a462c7264bd38a698fa3c551a528352f1d1d
Noah <cramernoah8@gmail.com>
2024-09-03 21:17:11 -0400

    adding scripts!
    the "/help" command works and i got the text to add to the text history label

--- learning01/main.gd
+++ learning01/main.gd
@@ -1,25 +1,37 @@
 extends Control
 
-@onready var text_history = $Panel2/VBoxContainer/Label
-
+@onready var text_history = $Panel2/RichTextLabel
+@onready var text_edit = $LineEdit
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	var name = "no_name"
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
 func _on_line_edit_text_submitted(new_text):
 	var _text_historystr = str(text_history)
-
+	
 	
 	if new_text == "/help":
+		text_edit.clear()
 		print("command /help typed")
+		
+		text_history.text =text_history.text + "\n\n/help command used\n\nList of commands:\n/help\n/name_player\n/search_room\n/end_game\n" 
+		
+		
 	elif new_text == "/name_player":
+		text_edit.clear()
 		print("start to name the player")
+		if new_text == "noah":
+			print("welcome dev")
 	elif new_text == "/search_room":
+		text_edit.clear()
 		print("you look around the room")
 	elif new_text == "/end_game":
+		text_edit.clear()
 		get_tree().quit()
 	else: 
+		text_edit.clear()
 		print("thats not a command")
